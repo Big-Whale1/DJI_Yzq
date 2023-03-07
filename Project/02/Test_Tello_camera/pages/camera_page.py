8f9f0ff1d4d218c00df1b7355277eddb1d6d19ec
Yzq <big.whale8888@gmail.com>
2023-03-07 17:39:47 +0800

    项目文件

--- Project/02/Test_Tello_camera/pages/camera_page.py
+++ Project/02/Test_Tello_camera/pages/camera_page.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:11a9ea966464612022ca0cf8bab14f79871bcf068adc3a15fb7de429ff6713a3
-size 1360
+oid sha256:044e19bde382d0ca405cc96f7886ab514c1d30fadfe31dd34b9761b389e11af3
+size 1400
